71b4f3c5394083e82a53c00e752cfe4b5e0234e4
TresorHZ <tresorzossoungbo06@gmail.com>
2026-05-27 11:01:05 +0100

    CORS configuré - liaison frontend/backend opérationnelle

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,11 +1,8 @@
 <?php
 
-use App\Http\Middleware\HandleAppearance;
-use App\Http\Middleware\HandleInertiaRequests;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,9 +12,13 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-    $middleware->alias([
-        'role' => \App\Http\Middleware\CheckRole::class,
-    ]);
+        $middleware->alias([
+            'role' => \App\Http\Middleware\CheckRole::class,
+        ]);
+
+        $middleware->prepend(
+            \Illuminate\Http\Middleware\HandleCors::class,
+        );
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
